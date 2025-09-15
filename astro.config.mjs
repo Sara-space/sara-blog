4d57e98a9040eeb64d766a87031b1f16fd09fe96
Sara Neftalí Martínez García <79623749+Sara-space@users.noreply.github.com>
2025-09-14 19:55:26 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://github.com/Sara-space",
+  site: "https://sara-space.github.io",
   base: "/sara-blog",                     
   integrations: [tailwind({ applyBaseStyles: false })],
 });
