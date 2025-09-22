406331ca83095b754180974fbe751f26eb26e79c
mrqm30 <mrqm06@gmail.com>
2025-09-21 21:45:49 -0600

    Corrijo rutas y actualizo versión del blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
+
 
 export default defineConfig({
-  site: "https://github.com/Sara-space",
-  base: "/",                     
-  integrations: [tailwind({ applyBaseStyles: false })],
+  site: 'https://mrqm30.github.io',
+  base: "/sara-blog",
+  trailingSlash: 'never',
+    integrations: [
+    tailwind(),
+    sitemap()
+  ],
 });
 
 
